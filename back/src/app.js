c880a7aea025fc140ebe1d7a218ce5b0538bbb88
baptiste <baptiste1.dumont@gmail.com>
2020-03-20 17:09:42 +0100

    try to save json file

--- back/src/app.js
+++ back/src/app.js
@@ -1,12 +1,73 @@
+class Parser {
+
+    constructor() {
+        this.fileParsed = null;
+        this.fileName = null;
+    }
+
+    doParse(filename) {
+
+        var fileJson = fs.readFile('./' + filename, 'utf8', (err, jsonString) => {
+            this.fileParsed = jsonString;
+            this.fileName = filename;
+            if (err) {
+                console.log("File read failed:", err)
+                return
+            }
+        })
+    }
+
+    async addToFile(data = null, filename = null)
+    {
+        if(filename == null)
+        {
+            this.fileName = "new_file-"+Date.now()+".json";
+        } else {
+            this.fileName = filename;
+        }
+
+        if(data === null)
+        {
+            data = {};
+        }
+
+
+
+
+        let datae = JSON.stringify(data, null, 2);
+
+        await this.doParse(filename);
+
+        if(this.fileParsed !== null)
+        {
+
+            let test = this.fileParsed;
+            datae = datae.concat(test);
+        }
+        this.fileParsed = datae;
+
+
+        fs.writeFile(this.fileName, datae, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+        });
+    }
+}
+
+
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const fs = require('fs');
 
 const documents = {}; //channel bdd
 
 var curChannel = 'general';
 var people = {};
 var channelList = {};
+let parse = new Parser();
+
 
 io.on("connection", socket => {
     let previousId;
@@ -95,10 +156,41 @@ io.on("connection", socket => {
     });
 
     io.emit("documents", Object.keys(documents));
+
     socket.on('new-message', (message) => {
         console.log(message);
         io.to(message.id).emit('new-message', message.message);
+
+
+        parse.doParse("data.json");
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        var h = today.getHours()
+
+        today = mm + '/' + dd + '/' + yyyy + ' ' + h;
+        var data = {};
+        data[message.id] = {
+                date: today,
+                channel: message.id,
+                message: message.message,
+                by: message.username
+        };
+
+        console.log(parse.fileParsed);
+
+        fs.appendFileSync('data.json', JSON.stringify(data, null, 2));
+        // parse.addToFile({
+        //     date: today,
+        //     channel: message.id,
+        //     message: message.message,
+        //     by: message.username
+        // }, "data.json");
+
+
     });
 });
+
 
 http.listen(4444);
