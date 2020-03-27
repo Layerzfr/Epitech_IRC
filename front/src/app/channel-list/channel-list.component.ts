8c983297f246d1216ddd49fc51b0e1598fe436b2
Radatovic <radatovic.damien@gmail.com>
2020-03-27 15:47:35 +0100

    edit name channel function

--- front/src/app/channel-list/channel-list.component.ts
+++ front/src/app/channel-list/channel-list.component.ts
@@ -7,64 +7,64 @@ import {userName} from "../connexion/connexion.component";
 export let currentDoc: string;
 
 @Component({
-    selector: 'app-document-list',
-    templateUrl: './channel-list.component.html',
-    styleUrls: ['./channel-list.component.scss']
+  selector: 'app-document-list',
+  templateUrl: './channel-list.component.html',
+  styleUrls: ['./channel-list.component.scss']
 })
 
 export class ChannelListComponent implements OnInit, OnDestroy {
-    documents: Observable<{}[]>;
-    currentDoc$: string;
-    bool: any;
-    isChecked: number = 0;
-    hex: any = '#';
-    newHex: any = '#';
-    id: any;
-    colors = {};
-    private _docSub: Subscription;
+  documents: Observable<{}[]>;
+  currentDoc$: string;
+  bool: any;
+  isChecked: number = 0;
+  hex: any = '#';
+  newHex: any = '#';
+  id: any;
+  colors = {};
+  private _docSub: Subscription;
+  active: number;
 
-    constructor(private channelService: ChannelService) {
-    }
+  constructor(private channelService: ChannelService) {
+  }
 
-    ngOnInit() {
-        this.documents = this.channelService.documents;
-        this._docSub = this.channelService.currentDocument.subscribe(doc => this.currentDoc$ = doc.id);
-        currentDoc = this.currentDoc$;
-    }
+  ngOnInit() {
+    this.documents = this.channelService.documents;
+    this._docSub = this.channelService.currentDocument.subscribe(doc => this.currentDoc$ = doc.id);
+    currentDoc = this.currentDoc$;
+  }
 
-    ngOnDestroy() {
-        this._docSub.unsubscribe();
-    }
+  ngOnDestroy() {
+    this._docSub.unsubscribe();
+  }
 
-    loadDoc(id: string) {
-        console.log(id);
-        currentDoc = id;
-        this.channelService.getDocument(id);
-    }
+  loadDoc(id: string) {
+    console.log(id);
+    currentDoc = id;
+    this.channelService.getDocument(id);
+  }
 
-    editDoc(id: string, newId) {
-        console.log(id);
-        currentDoc = id;
-        this.channelService.editDocument(id, newId.target.value);
-    }
+  editDoc(id: string, newId) {
+    currentDoc = id;
+    this.channelService.editDocument(id, newId.target.value);
+  }
 
-    newDoc() {
-        this.channelService.newDocument();
-    }
+  newDoc() {
+    this.channelService.newDocument();
+  }
 
-    generateHex(channel:any) {
-      let hex = '#';
-        if (!this.colors[channel]) {
-          console.log(hex);
-          let length = 6;
-          let chars = '0123456789ABCDEF';
-          while (length--) {
-              hex += chars[(Math.random() * 16) | 0];
-          }
-          this.colors[channel] = hex;
-        }
-        return this.colors[channel];
+  generateHex(channel: any) {
+    let hex = '#';
+    if (!this.colors[channel]) {
+      console.log(hex);
+      let length = 6;
+      let chars = '0123456789ABCDEF';
+      while (length--) {
+        hex += chars[(Math.random() * 16) | 0];
+      }
+      this.colors[channel] = hex;
     }
+    return this.colors[channel];
+  }
 
   checkBox(DocId) {
     this.bool = document.getElementById(DocId);
@@ -76,13 +76,16 @@ export class ChannelListComponent implements OnInit, OnDestroy {
       this.isChecked = 0
     }
     if (DocId == 'general') {
-        this.isChecked = 1;
+      this.isChecked = 1;
     }
   }
 
-
-    dynamicStyles() {
-
+  changeInputStatus(id: any) {
+    if (this.active === 1) {
+      this.active = 0;
+    } else {
+      this.active = 1;
     }
+  }
 
 }
